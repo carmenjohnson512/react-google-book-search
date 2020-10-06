1ae933c2c4527cc4e31b0bb054c7040f9200ed2c
Carmen Johnson <carmenjohnson512@gmail.com>
2020-10-06 01:13:40 -0500

    re-adding build files, heroku deployment build failed again

---
+++ client/build/precache-manifest.cc543733bf487b5d5a326d179532d27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6cd4b184fe58edd119d",
+    "url": "/static/css/main.25fb519f.chunk.css"
+  },
+  {
+    "revision": "f6cd4b184fe58edd119d",
+    "url": "/static/js/main.8acaedf8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58a56a4620ebcc42f685",
+    "url": "/static/js/2.1a5d0118.chunk.js"
+  },
+  {
+    "revision": "e2646e4c3526e2c07f29330b22d49bd4",
+    "url": "/static/media/books.e2646e4c.jpg"
+  },
+  {
+    "revision": "7a52ade5bb18828dfb83bf320ddfdda1",
+    "url": "/index.html"
+  }
+];
